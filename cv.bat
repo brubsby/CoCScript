15bd36c87f1a95fce44d2780832a991ddf8d4478
typicalTYLER <TylerBusby333@gmail.com>
2014-04-30 09:37:19 -0500

    Fixed computer vision batch
    now uses relative paths

--- cv.bat
+++ cv.bat
@@ -1,4 +1,4 @@
-for /f %%f in ('dir /b E:\GitHub\CoCScript\BasePictures') do (
-	convert E:\GitHub\CoCScript\BasePictures\%%f -negate -threshold 60%% -edge 5 -morphology EdgeOut Disk -fill white -floodfill +20+425 black -floodfill +20+375 black -floodfill +20+325 black -floodfill +20+275 black -floodfill +20+225 black -floodfill +1100+425 black -floodfill +1100+375 black -floodfill +1100+325 black -floodfill +1100+275 black -floodfill +1100+225 black E:\GitHub\CoCScript\Filtered\%%f
-	convert E:\GitHub\CoCScript\Filtered\%%f -format %%c histogram:info:- >E:\GitHub\CoCScript\Histograms\%%f.txt
+for /f %%f in ('dir /b %~dp0\BasePictures') do (
+	convert %~dp0\BasePictures\%%f -negate -threshold 60%% -edge 5 -morphology EdgeOut Disk -fill white -floodfill +20+425 black -floodfill +20+375 black -floodfill +20+325 black -floodfill +20+275 black -floodfill +20+225 black -floodfill +1100+425 black -floodfill +1100+375 black -floodfill +1100+325 black -floodfill +1100+275 black -floodfill +1100+225 black %~dp0\Filtered\%%f
+	convert %~dp0\Filtered\%%f -format %%c histogram:info:- >%~dp0\Histograms\%%f.txt
 )
